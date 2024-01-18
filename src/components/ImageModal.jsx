44532daa089ea268cd77b83d5852ee6c67ae989f
BaloyiTk <tiyanikevinbaloyi@gmail.com>
2024-01-18 08:58:07 +0200

    update

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -1,12 +1,16 @@
 // ImageModal.js
 import React from "react";
 
+const preventRightClick = (event) => {
+  event.preventDefault();
+};
+
 const ImageModal = ({ imageUrl, closeModal }) => {
   return (
     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4 max-w-screen-lg h-96 max-h-screen">
       <div className="relative w-full h-full">
         <div
-          className="absolute top-4 right-4 cursor-pointer  text-xl z-50 text-orange-500 hover:text-gray-800 font-bold"
+          className="absolute top-4 right-4 cursor-pointer text-xl z-50 text-orange-500 hover:text-gray-800 font-bold"
           onClick={closeModal}
         >
           X
@@ -15,7 +19,8 @@ const ImageModal = ({ imageUrl, closeModal }) => {
           src={imageUrl}
           alt="Enlarged Image"
           className="w-full h-full object-cover border-2 border-white rounded-md"
-		  onClick={closeModal}
+          onClick={closeModal}
+          onContextMenu={preventRightClick}
         />
       </div>
     </div>
@@ -23,3 +28,4 @@ const ImageModal = ({ imageUrl, closeModal }) => {
 };
 
 export default ImageModal;
+
